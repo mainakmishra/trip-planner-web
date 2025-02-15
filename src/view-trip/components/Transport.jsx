533e601a767fed2041a4bd282b1faa101de8f797
mainakmishra <mainakmishra00@gmail.com>
2025-02-15 08:16:08 +0530

    updated Transport

--- src/view-trip/components/Transport.jsx
+++ src/view-trip/components/Transport.jsx
@@ -7,8 +7,8 @@ function Transport({tripInfo}) {
     <div className='container mx-auto py-8'>
         <h2 className='font-bold text-2xl md:text-3xl text-gray-800 mb-6'>Travel</h2>
         <div>
-            <h2 className='font-bold text-xl mb-4 text-gray-800 mt-2'>Distance by road : {tripInfo?.tripData?.distancebyRoad}</h2>
-            <h2 className='font-bold text-xl mb-4 text-gray-800 mt-2'>Time to travel : {tripInfo?.tripData?.timetotravelbyRoad}</h2>
+            <h2 className='font-bold text-xl mb-4 text-gray-800 mt-2'>Distance by road : {tripInfo?.tripData?.distanceByRoad}</h2>
+            <h2 className='font-bold text-xl mb-4 text-gray-800 mt-2'>Time to travel : {tripInfo?.tripData?.timeTotravelByRoad}</h2>
             <TrainCard tripInfo={tripInfo}/>
             <FlightCard tripInfo={tripInfo}/>
         </div>
