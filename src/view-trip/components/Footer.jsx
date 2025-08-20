aada9add10d85554d15208ca21809fc2295696e3
Mainak Mishra <mainakmishra00@gmail.com>
2025-08-20 21:49:41 +0530

    changed footer

--- src/view-trip/components/Footer.jsx
+++ src/view-trip/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div>
-        <h2 className='text-center text-gray-400'>Created by Mainak Mishra</h2>
+        <h2 className='text-center text-gray-400'>Created with React</h2>
     </div>
   )
 }
