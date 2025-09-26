cbe1cf9997d2a4d03414505831fcc873d163d9b1
Mainak Mishra <mainakmishra@Mainaks-MacBook-Air.local>
2025-09-26 13:51:09 +0530

    refactoring changes

--- src/services/firebaseconfig.jsx
+++ src/services/firebaseconfig.jsx
@@ -4,13 +4,13 @@ import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4TfDFvEh8PbOpjuy4hVuuUz2RTbsI9ME",
-  authDomain: "ai-trip-planner-f31d9.firebaseapp.com",
-  projectId: "ai-trip-planner-f31d9",
-  storageBucket: "ai-trip-planner-f31d9.firebasestorage.app",
-  messagingSenderId: "719404761599",
-  appId: "1:719404761599:web:1eb3f40dcf4f0cbef167ca",
-  measurementId: "G-C7ZKCQP285"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 
